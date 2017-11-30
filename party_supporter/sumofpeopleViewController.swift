fcf9533cdbacaad3bd71ad1a4fa92ae5180857ee
Motoharu Kumanomido <e165720@.u-ryukyu.ac.jp>
2017-11-30 20:12:45 +0900

    UIとコードの紐ずけをした．

---
+++ party_supporter/sumofpeopleViewController.swift
@@ -0,0 +1,36 @@
+//
+//  sumofpeopleViewController.swift
+//  party_supporter
+//
+//  Created by 熊埜御堂元晴 on 2017/11/30.
+//  Copyright © 2017年 Univ-ryukyu. All rights reserved.
+//
+
+import UIKit
+
+class sumofpeopleViewController: UIViewController{
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
